388d5a88cefe646f0a40a441f9911c912991c100
Everesh <everesh@proton.me>
2024-05-10 23:20:58 +0200

    Use Ruby build-in csv parser

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,8 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 
-lines = File.readlines('event_attendees.csv')
-lines.each.with_index do |line, index|
-  next if index.zero?
-
-  columns = line.split(',')
-  name = columns[2]
-  p name
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
+  puts name
 end
