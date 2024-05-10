2d0ee65a24df3bb0f6122f2525ef22b2d8693c75
Everesh <everesh@proton.me>
2024-05-10 23:58:40 +0200

    Adjust legislator_names formating

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -23,7 +23,7 @@ contents.each do |row|
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     )
     legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
+    legislator_names = legislators.map(&:name).join(', ')
   rescue
     legislator_names = 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
