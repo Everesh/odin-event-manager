6d3c6abbc7938810500a9c99b01e5671dfc1b72f
Everesh <everesh@proton.me>
2024-05-11 00:29:57 +0200

    Redirect output to individual files

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -27,6 +27,7 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
@@ -34,5 +35,12 @@ contents.each do |row|
   legislator_names = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
+
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
