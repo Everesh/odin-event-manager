90298a1921985abb87907791ba5e6be4617ae275
Everesh <everesh@proton.me>
2024-05-11 00:31:31 +0200

    Extract save_thank_you_letter into  method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -17,6 +17,16 @@ def legislators_by_zipcode(zipcode)
   end
 end
 
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -36,11 +46,5 @@ contents.each do |row|
 
   form_letter = erb_template.result(binding)
 
-  Dir.mkdir('output') unless Dir.exist?('output')
-
-  filename = "output/thanks_#{id}.html"
-
-  File.open(filename, 'w') do |file|
-    file.puts form_letter
-  end
+  save_thank_you_letter(id,form_letter)
 end
